909c65ea274e1c8926cac40d3b9ab717aed731cb
Ryan J Zafft <123418615+rzafft1@users.noreply.github.com>
2024-10-13 14:26:43 -0600

    Update christmas_process.cpp

--- christmas_process.cpp
+++ christmas_process.cpp
@@ -62,7 +62,7 @@ void Santa(){
         printf("\n(Update) It is christmas time, santa and the reindeer are going off to work for %d seconds...\n\n", christmas_time);
         sleep(christmas_time);
         printf("\n(Update) Christmas time is over, santa is going to sleep, and the reindeers are going on vacation...\n\n", christmas_time);
-        sem_post(&christmas);
+        sem_init(&christmas, 0, 9);
     }
 }
 
